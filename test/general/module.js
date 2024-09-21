642c2e541590828b28306df90a4334963255ed45
Daniel Duarte <danieldd.ar@gmail.com>
2024-09-20 22:21:07 -0300

    chore: re-organize tests and built-in functions

--- test/general/module.js
+++ test/general/module.js
@@ -6,8 +6,8 @@ describe('Module', function () {
   it('has expected interface', function () {
     const module = require('../..');
 
-    const actualInterface = typeof module;
-    const expectedInterface = 'function';
+    const actualInterface = [typeof module, module.length];
+    const expectedInterface = ['function', 3]; // The module is a function with 3 parameters
 
     assert.deepStrictEqual(actualInterface, expectedInterface);
   });
--- test/index.js
+++ test/index.js
@@ -1,10 +1,18 @@
 // Test files in order of execution
+
+// -- General
 require('./general/module');
+
+// -- Literals
 require('./expressions/literals');
+
+// -- Operators
 require('./operators/unary-ops');
 require('./operators/binary-ops');
 require('./operators/ternary-ops');
 require('./operators/short-circuit');
+
+// -- Functions
 require('./functions/lambda-basic');
 require('./functions/lambda-scopes');
 require('./functions/lambda-code-block');
@@ -13,24 +21,36 @@ require('./functions/func-exps-basic');
 require('./functions/func-exps-code-block');
 require('./functions/func-exps-scopes');
 require('./functions/func-exps-methods');
+
+// -- Expressions
 require('./expressions/nested');
 require('./expressions/objects');
+require('./expressions/spread');
+
+// -- Variables and constants
 require('./vars/const');
 require('./vars/let');
 require('./vars/assign');
 require('./vars/user-vars');
+require('./vars/closures');
+
+// -- Built-ins
 require('./built-in/string');
 require('./built-in/array');
 require('./built-in/object');
+require('./built-in/json');
+require('./built-in/math');
+require('./built-in/functions');
+
+// -- Special expressions
 require('./general/this');
+
+// -- Statements
 require('./statements/blocks');
 require('./statements/if');
 require('./statements/while');
 require('./statements/for-of');
-require('./general/random-expressions');
+
+// -- Other
 require('./general/halting');
-require('./built-in/json');
-require('./built-in/math');
-require('./built-in/functions');
-require('./expressions/spread');
-require('./vars/closures');
+require('./general/random-expressions');
